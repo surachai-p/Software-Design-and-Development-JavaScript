2e6aae573653b917f61ca3689571668352724145
Chutiporn120846 <67030059@kmitl.ac.th>
2025-02-11 16:34:52 +0700

    gg

--- javatest/lab2.1.js
+++ javatest/lab2.1.js
@@ -1,14 +1,15 @@
 // ประกาศตัวแปรสำหรับเก็บข้อมูลนักศึกษา
 let studentId = "67030059";       
 let studentName = "ชุธิพร เชาว์ชัย";      
-let midTermScore = 75;           
-let finalTermScore = 80;         
+let midTermScore = 20;           
+let finalTermScore = 40;         
 
-// คำนวณเกรดเฉลี่ย
 const GPA = 3.55   // เกรดเฉลี่ย (ใช้ const เพราะค่าไม่ควรเปลี่ยนแปลงหลังจากคำนวณ)
 
-console.log(`รหัสนักศึกษา: ${studentId}`);
-console.log(`ชื่อ: ${studentName}`);
-console.log(`คะแนนสอบกลางภาค: ${midTermScore}`);
-console.log(`คะแนนสอบปลายภาค: ${finalTermScore}`);
-console.log(`เกรดเฉลี่ย: ${GPA}`);
+let ST = {
+    studentId: "67030059",
+    studentName: "ชุธิพร เชาว์ชัย",
+    midTermScore: "20",
+    finalTermScore: "40",
+    GPA: "3.55"
+};
